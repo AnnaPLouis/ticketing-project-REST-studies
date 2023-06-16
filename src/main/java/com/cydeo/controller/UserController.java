0f44c665370c9d634585d5a73fada3d773dd7f5c
Anna Pierre-Louis <anitataksa3440@gmail.com>
2023-06-16 13:45:52 -0400

    Logging

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.annotation.ExecutionTime;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.exception.TicketingProjectException;
@@ -25,6 +26,7 @@ public class UserController {
     }
 
 
+    @ExecutionTime
     @GetMapping
     @RolesAllowed({"Manager", "Admin"})
     @Operation(summary = "Get users")
@@ -34,6 +36,7 @@ public class UserController {
                 userService.listAllUsers(), HttpStatus.OK));
     }
 
+    @ExecutionTime
     @GetMapping("/{name}")
     @RolesAllowed("Admin")
     @Operation(summary = "Get user by username")
